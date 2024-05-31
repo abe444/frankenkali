56dd6f0a165644bb967b9855dad27d8e3f9b4c2d
abe444 <spacedragon314@hotmail.com>
2024-05-31 12:00:44 -0500

    Better structure...

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
-import os
+import subprocess
 
-os.system('apt install vim zap ffuf sublist3r curl nmap git')
-os.system('mkdir ~/.testdir')
-os.system('mkdir ~/testdir2')
+
+def packageHandler(count):
+    listLength = len(count)
+    print(f"\nInstalling {listLength} packages. Standby...\n")
+
+
+# Enter your tools of choice here.
+tools = ["curl",
+         "ffuf",
+         "git",
+         "nmap",
+         "python3-shodan",
+         "python3-pip",
+         "sublist3r",
+         "vim",
+         "zaproxy"
+         ]
+
+
+print(tools)
+packageHandler(tools)
+
+for tool in tools:
+    try:
+        subprocess.check_call(['apt-get', 'install', '-y', tool])
+    except subprocess.CalledProcessError as e:
+        print(f"Error installing {tool}: {e}")
+        try:
+            subprocess.check_call(['snap', 'install', tool])
+        except subprocess.CalledProcessError as e:
+            print(f"Error installing {tool} using snap: {e}")
